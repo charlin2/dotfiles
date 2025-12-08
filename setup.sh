2e627c4109affd3b9e536fdc7269e551cc19e02b
Charlie Lin <charliel@palantir.com>
2025-12-07 21:20:58 -0500

    .

--- setup.sh
+++ setup.sh
@@ -6,16 +6,15 @@ VIMPACK="$HOME/.vim-plugins/pack/plugins/start"
 mkdir -p "$VIMPACK"
 
 installPlugin() {
-    repo="$1"
-    name="$2"
+  repo="$1"
+  name="$2"
 
-    if [ ! -d "$VIMPACK/$name" ]; then
-        echo "Installing $name..."
-        git clone --depth 1 "$repo" "$VIMPACK/$name"
-    else
-        echo "$name already installed, skipping"
-    fi
+  if [ ! -d "$VIMPACK/$name" ]; then
+    echo "Installing $name..."
+    git clone --depth 1 "$repo" "$VIMPACK/$name"
+  else
+    echo "$name already installed, skipping."
+  fi
 }
 
 installPlugin https://github.com/catppuccin/vim catppuccin
-
