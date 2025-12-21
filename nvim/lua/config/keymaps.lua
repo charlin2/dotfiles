4396afe49736820bbd192fdcd480833576f4829e
charlin2 <clin10900@gmail.com>
2025-12-21 17:13:29 -0500

    .

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,6 +6,9 @@
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = "Next buffer" })
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = "Previous buffer" })
 
+-- Open floating diagnostic message
+vim.keymap.set("n", "<leader>dh", vim.diagnostic.open_float, { desc = "View diagnostic" })
+
 -- Open terminal on right vertical split, nvim root dir
 vim.keymap.set("n", "<leader>ft", function()
   Snacks.terminal(nil, {
@@ -17,3 +20,6 @@ vim.keymap.set("n", "<leader>ft", function()
     auto_insert = true,
   })
 end, { desc = "Terminal right" })
+
+vim.keymap.set("n", "<leader>q", "@q", { desc = "Run q macro" })
+
