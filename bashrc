16aadda3c55329720632131110908645e255eed5
charlin2 <clin10900@gmail.com>
2025-12-21 16:57:08 -0500

    .

--- bashrc
+++ bashrc
@@ -1,10 +1,13 @@
 export PS1="\[\033[38;5;76m\]\u \[\033[0m\]\W\$ "
+export EDITOR="nvim"
+export VISUAL="$EDITOR"
 
-alias ls='ls -la --color=auto'
+alias ls='ls -l --color=auto'
 alias ff='fastfetch'
 alias vim='nvim'
 alias python='python3'
 alias pip='pip3'
+alias grep='grep --color=auto'
 
 if [[ $- == *i* && -z "$NVIM" && -z "$VIM_TERMINAL" ]]; then
   fastfetch
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Cycle through buffers
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = "Next buffer" })
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = "Previous buffer" })
+
 -- Open terminal on right vertical split, nvim root dir
 vim.keymap.set("n", "<leader>ft", function()
   Snacks.terminal(nil, {
