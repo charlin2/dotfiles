5b18cc8ac14d804cf087e9d6424f290362e78fca
charlin2 <clin10900@gmail.com>
2025-12-23 16:26:29 -0500

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,10 @@ require("config.go_env")
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Set cpp standard
+require('lspconfig').clangd.setup {
+  init_options = {
+    fallbackFlags = {'--std=c++20'}
+  },
+}
