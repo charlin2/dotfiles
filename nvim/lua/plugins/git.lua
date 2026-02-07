986839ad57a922c7559a85308e16c85de9e32d4a
charlin2 <clin10900@gmail.com>
2026-02-07 10:45:45 -0500

    .

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -23,7 +23,7 @@ return {
         delay = 300,
         ignore_whitespace = false,
       },
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> | <summary>",
     },
     keys = {
       { "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle inline blame" },
